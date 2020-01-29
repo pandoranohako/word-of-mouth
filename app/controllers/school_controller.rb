a32901b5b4e7d8c924052085347c178f49ef403f
lies046 <skytree.skysky@gmail.com>
2020-01-29 18:39:29 +0900

    add

--- app/controllers/school_controller.rb
+++ app/controllers/school_controller.rb
@@ -1,2 +1,34 @@
 class SchoolController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+      if @post.save
+        redirect_to root_path 
+    else
+        render :new
+    end
+  end
+
+  def show
+    @school = School.find(params[:id])
+    @post = Post.find(params[:id])
+    @admission = @post.admission
+    @merit = @post.merit
+    @demerit = @post.demerit
+    @cost = @post.cost
+    @gap = @post.gap
+    @curriculum_qualit = @post.curriculum_quality
+    @out_of_learning = @post.out_of_learning
+    @employment = @post.employment
+  end
+  private
+
+  def post_params
+    params.require(:post).permit(
+      :admission, :merit, :demerit, :cost, :gap, :curriculum_quality, :out_of_learning, :employment,:school_id)
+  end
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,4 @@
 class School < ApplicationRecord
   mount_uploader :image, ImageUploader
+  belongs_to :post,optional: true
 end
