8d954bde529d799cac218829e661903d15101fdd
pandoranohako <m.o_5963@icloud.com>
2020-01-29 19:05:35 +0900

    セレクトボックスの実装

--- app/controllers/school_controller.rb
+++ app/controllers/school_controller.rb
@@ -1,7 +1,36 @@
 class SchoolController < ApplicationController
+  # before_action :set_schools, only: [:new, :create]
+
+  def new
+    @school = School.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+      if @post.save
+        redirect_to root_path 
+    else
+        render :new
+    end
+  end
+
   def show
     @school = School.find(params[:id])
   end
+
+  private
+
+  # def set_schools
+  #   @schools = School.all
+  # end
+
+  def post_params
+    params.require(:post).permit(
+      :admission, :merit, :demerit, :cost, :gap, :curriculum_quality, :out_of_learning,  :employment,  :school_id
+      )
+
+  end
+
 
 
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   validates :admission,  :merit,  :demerit,  :gap,  :curriculum_quality,  :out_of_learning,  :employment, :cost ,presence: true
   
   has_many :users
-  has_many :schools
+  belongs_to :school, foreign_key: 'school_id', optional: true
+  
 end
