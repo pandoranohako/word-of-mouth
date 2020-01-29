f05be7d7b10112265afdc02942e41ea14e9342d4
lies046 <skytree.skysky@gmail.com>
2020-01-29 12:20:00 +0900

    修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
   validates :admission,  :merit,  :demerit,  :gap,  :curriculum_quality,  :out_of_learning,  :employment, :cost ,presence: true
-  
   has_many :users
   has_many :schools
 end
