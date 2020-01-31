5284ee6dcb6e073fa355b826944cb6f695f62449
shun-pro <@shun.80367246@gmail.com>
2020-01-31 12:52:12 +0900

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_042402) do
+ActiveRecord::Schema.define(version: 2020_01_29_051218) do
 
   create_table "inquiries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
