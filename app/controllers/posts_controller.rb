e1fe39f7f886b0fedb42985ce6545c6a2a29e38d
pandoranohako <m.o_5963@icloud.com>
2020-01-31 19:06:56 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,20 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+      if @post.save
+        redirect_to root_path 
+    else
+        render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(
+      :admission, :merit, :demerit, :cost, :gap, :curriculum_quality, :out_of_learning,  :employment,  :school_id).merge(user_id: current_user.id)
+  end
 end
